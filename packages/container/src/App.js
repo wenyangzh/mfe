d527b7fc2b835858ac8f68c9b61cc7dc684ed181
wzhang <wzhang@3vgeomatics.com>
2025-07-11 11:42:12 -0700

    Add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+
 export default () => {
-  return <div>
-    <div>hiiii</div>
-    <MarketingApp />
-  </div>
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  )
 }
